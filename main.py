2da750ceb8a3f8be6104629f5d4e88e643f67e1e
kalomano <sowerpablo@gmail.com>
2022-09-29 18:25:59 +0200

    AudioRecorderAdded
    I added an audio record function from microphone (for the moment i will sue my own microphone)

--- main.py
+++ main.py
@@ -3,6 +3,11 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import whisper
+import wave
+import pyaudio
+import time
+import threading
+import concurrent.futures
 
 
 def print_hi(name):
@@ -21,12 +26,74 @@ def getReady():
     return model_ready
 
 
+def waitSeconds(nSeconds):
+    time.sleep(nSeconds)
+
+
+def startRecord(recordX,time):
+    print("recording "+ recordX)
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+    frames = []
+
+    #Creating a thread to take the audio while it is running (used only to take aduio for Xseconds)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        f1 = executor.submit(waitSeconds,time)
+        while f1.running():
+            data = stream.read(1024)
+            frames.append(data)
+
+    print("Audio grabado")
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    sound_file = wave.open((recordX+".mp3"), "wb")
+    sound_file.setnchannels(1)
+    sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+    sound_file.setframerate(44100)
+    sound_file.writeframes(b''.join(frames))
+    sound_file.close()
+
+
+def transcribe(modelUsed,name):
+    result = model.transcribe(name+".mp3")
+    return result["text"]
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    #GET READY
     model = getReady()
-    result = model.transcribe("shortAudio.mp3")
-    print(result["text"])
+    voiceSeconds = []
+    voiceSecondsIndex = 0
+    secondsWait = 2
+
+    #APP CICLE CODE (each secondsWait seconds)
+    while True:
+        print("Siguiente 'segundos'")
+
+        #Record the secondsWait duration audio
+        threadRecorder = threading.Thread(target=startRecord, args=(("input"+str(voiceSecondsIndex)),secondsWait))
+        threadRecorder.start()
+        threadRecorder.join()
+
+        #is it nonsound?
+        prevAudioTxt = transcribe(model,"input"+ str(voiceSecondsIndex))
+        print (prevAudioTxt)
+        if(prevAudioTxt != ""):
+            print(" ALGO HAY")
+            # Next "seconds"
+            voiceSecondsIndex += 1
+        else:
+            print("NADA HAY")
+            # Return to 0 "seconds"
+            voiceSecondsIndex = 0
 
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
