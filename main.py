366202b02b237887f1e63d3e0dfc8e0541e5f015
kalomano <sowerpablo@gmail.com>
2022-09-29 12:32:47 +0200

    Initial whisper setup
    Just importing whisper

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import whisper
 
 
 def print_hi(name):
@@ -9,8 +10,23 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def getReady():
+    print("preparing initial data, don't talk yet.")
+
+    #Get the whisper model we want to use (Affects perfomance)
+    model_ready = whisper.load_model("tiny")
+
+
+
+    return model_ready
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    model = getReady()
+    result = model.transcribe("shortAudio.mp3")
+    print(result["text"])
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
